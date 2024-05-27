2f5e96b0ebc02bdfd9f188693db320ab6a422118
IrynaLebedenko <94801312+IrynaLebedenko@users.noreply.github.com>
2024-05-27 21:41:23 +0300

    create hw

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,19 @@
-import { PATH_DB } from '../constants/contacts.js';
 
-export const addOneContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import { createFakeContact} from'../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+    try{
+       let data = await fs.readFile(PATH_DB, 'utf-8');
+       const contacts = JSON.parse(data);
+       const newContact = createFakeContact();
+
+       contacts.push(newContact);
+       await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    } catch (error){
+        console.log(error);
+    }
+};
 
 await addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import { createFakeContact} from'../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+export const generateContacts = async (number) => {
+    try {
+        let data = await fs.readFile(PATH_DB, 'utf-8');
+        const contacts = JSON.parse(data);
+        for (let i = 0; i < number; i++) {
+            contacts.push(createFakeContact());
+        }
 
-await generateContacts(5);
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+await generateContacts( 5);
+
